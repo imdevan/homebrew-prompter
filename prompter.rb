52b0a99a5364ab6fb3b645f7e6f41bd49e2c6507
Devan Huapaya <huapayadevan@gmail.com>
2026-02-18 12:38:47 -0800

    update to v2.0.1

--- prompter.rb
+++ prompter.rb
@@ -1,17 +1,31 @@
 class Prompter < Formula
   desc "CLI tool for assembling AI coding prompts"
   homepage "https://github.com/imdevan/prompter"
-  url "https://github.com/imdevan/prompter/archive/refs/tags/v2.0.1.tar.gz"
-  sha256 "3057f848dfe19c9771aae293818addbd077a7ef38f6ec68692e7cfb98658e9f9"
   version "2.0.1"
 
-  depends_on "go" => :build
+  if OS.mac?
+    if Hardware::CPU.arm?
+      url "https://github.com/imdevan/prompter/releases/download/v2.0.1/prompter-darwin-arm64"
+      sha256 "0213df04cef09b8d32dfc9be2d960105ae0b0e1c498e9d753322ffb9d5beceec"
+    else
+      url "https://github.com/imdevan/prompter/releases/download/v2.0.1/prompter-darwin-amd64"
+      sha256 "21998b173024f7fa07b9f49790de01f8a442bd928c4554d376ea98a97aa9881c"
+    end
+  elsif OS.linux?
+    if Hardware::CPU.arm?
+      url "https://github.com/imdevan/prompter/releases/download/v2.0.1/prompter-linux-arm64"
+      sha256 "2a09b673d8cd73549e3be37e32a810586e83b331c14588df5926a390e802a1dc"
+    else
+      url "https://github.com/imdevan/prompter/releases/download/v2.0.1/prompter-linux-amd64"
+      sha256 "df80ea3d782f63c8d971c5e486408cab7a09c46f269ae2ae87eedeaeb42b1837"
+    end
+  end
 
   def install
-    system "go", "build", *std_go_args, "./cmd/prompter"
+    bin.install Dir["prompter-*"].first => "prompter"
   end
 
   test do
-    system "#{bin}/prompter", "--version"
+    system "\#{bin}/prompter", "--version"
   end
 end
